d5f48013b7d7b9df27799e17100791f2babd1ed6
Sukesh Aedla <sukesh1304@gmail.com>
2017-08-19 19:12:40 -0400

    update web server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -11,6 +11,7 @@ app.use(bodyparser.json());
 app.use(express.static(rootPath + '/app'));
 
 app.get('/data/event/:id', events.get);
+app.get('/data/event', events.getAll);
 app.post('/data/event/:id', events.save)
 
 app.listen(8000);
