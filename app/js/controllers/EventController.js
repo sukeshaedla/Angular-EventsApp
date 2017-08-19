40c91952c7d60b48ed22c70a6d006c1b6bfe42e3
Sukesh Aedla <sukesh1304@gmail.com>
2017-08-18 22:08:37 -0400

    upvotes and downvotes for events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -12,7 +12,41 @@ eventsApp.controller('EventController',
 				city: 'Mountain View',
 				state: 'CA'
 			},
-			imageUrl: '/img/angularjs-logo.png'
+			imageUrl: '/img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directive Masterclass',
+					creatorName: 'Aditya Vishal',
+					duration: '1 hour',
+					level: 'Abstract',
+					abstract: "In this session, you will learn the advanced concepts of directive",
+					upVoteCount: 0
+				},
+				{
+					name: 'Scopes for fun and profit',
+					creatorName: 'Sukesh Aedla',
+					duration: '30 minutes',
+					level: 'Introductory',
+					abstract: "In this session, we will take a closer look at scopes",
+					upVoteCount: 0
+				},
+				{
+					name: 'Well Behaved Controllers',
+					creatorName: 'Aditya Vishal',
+					duration: '2 hours',
+					level: 'Abstract',
+					abstract: "In this session, you will learn the advanced concepts of controllers",
+					upVoteCount: 0
+				}				
+			]
 		}
+
+		$scope.upVoteSession = function(session) {
+			session.upVoteCount++;
+		};
+
+		$scope.downVoteSession = function(session) {
+			session.upVoteCount--;
+		};		
 	}
 );
