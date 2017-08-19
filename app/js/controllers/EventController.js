39a1c05c9caaf1b531f7d9947a3baa957aa4b3ba
Sukesh Aedla <sukesh1304@gmail.com>
2017-08-19 11:03:21 -0400

    remove callback http service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,12 +1,14 @@
 'use strict';
 
 eventsApp.controller('EventController', 
-	function EventController($scope, eventData){
+	function EventController($scope, eventData, $log){
 
 		$scope.sortorder = 'name';
-		
-		eventData.getEvent(function(event){
-			$scope.event = event;
+
+		eventData.getEvent()
+		.success(function(event){$scope.event = event; })
+		.error(function(data, status, headers, config){
+			$log.warn(data, status, headers, config)
 		});
 
 		$scope.upVoteSession = function(session) {
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -3,13 +3,7 @@
 eventsApp.factory('eventData', function($http, $log){
 	return {
 		getEvent: function(successcb){
-			$http({method: 'GET', url: '/data/event/1'}).
-			success(function(data, status, headers, config){
-				successcb(data);
-			}).
-			error(function(data, status, headers, config){
-				$log.warn(data, status, headers, config)
-			});
+			return $http({method: 'GET', url: '/data/event/1'});
 		}
 	}
 })
