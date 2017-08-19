87760426b5b293e3a2c4288e896fdffbc5b2d51a
Sukesh Aedla <sukesh1304@gmail.com>
2017-08-19 12:36:58 -0400

    $resource usage post data

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,9 +1,14 @@
 'use strict';
 
 eventsApp.factory('eventData', function($resource){
+	var resource = $resource('/data/event/:id', {id: '@id'});
 	return {
 		getEvent: function(){
-			return $resource('/data/event/:id', {id: '@id'}).get({id:1});
+			return resource.get({id:1});
+		},
+		save: function(event){
+			event.id = 999;
+			return resource.save(event);
 		}
 	}
 })
